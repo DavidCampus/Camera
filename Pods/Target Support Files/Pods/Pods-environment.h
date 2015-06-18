18a9ec366706d5759df9c4aa1c69baec9411ee2c
Farrukh34 <farrukhkhan@Farrukhs-MacBook-Air-2.local>
2015-06-18 15:24:23 +0100

    Camera original

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// FLTextView
+#define COCOAPODS_POD_AVAILABLE_FLTextView
+#define COCOAPODS_VERSION_MAJOR_FLTextView 1
+#define COCOAPODS_VERSION_MINOR_FLTextView 0
+#define COCOAPODS_VERSION_PATCH_FLTextView 2
+
+// FLTextView/Default
+#define COCOAPODS_POD_AVAILABLE_FLTextView_Default
+#define COCOAPODS_VERSION_MAJOR_FLTextView_Default 1
+#define COCOAPODS_VERSION_MINOR_FLTextView_Default 0
+#define COCOAPODS_VERSION_PATCH_FLTextView_Default 2
+
